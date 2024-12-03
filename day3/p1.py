279989623e730d81f68d713220f8edf5f8ea10dc
Ben Kahl <ben.kahl2002@gmail.com>
2024-12-02 21:35:23 -0800

    day 3 part 1 finished

---
+++ day3/p1.py
@@ -0,0 +1,23 @@
+import re
+
+pattern = re.compile(r"mul\(\d*,\d*\)")
+param = re.compile(r"\d+")
+
+
+def parse_instruction(ins):
+    m1, m2 = param.findall(ins)
+    return int(m1) * int(m2)
+
+
+def perform_muls(instructions):
+    sum = 0
+    for ins in instructions:
+        sum += parse_instruction(ins)
+    return sum
+
+
+with open("input.txt", 'r') as file:
+    text = file.read()
+
+instructions = pattern.findall(text)
+print(perform_muls(instructions))
